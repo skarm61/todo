67a3f348d3490cc90958d741c6cc179da214f25c
Skarm <skarm61@gmail.com>
2012-09-13 11:03:40 +0300

    Редактировать список может тольког автор теперь-некоторые правки безопасности

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,7 +2,15 @@
   def done
     image_tag("done.png", :alt => "Sample App", :class => "round")
   end
-    def not_done
+  
+  def not_done
     image_tag("not_done.png", :alt => "Sample App", :class => "round")
   end
+  
+  def del
+    image_tag("del.png", :alt => "Sample App", :class => "round")
+  end
+  
+  
+  
  end
